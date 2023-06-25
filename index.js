5a97cb1f687789e4e35261a00d6ce916e1e00222
fzf404 <me@fzf404.art>
2023-06-25 13:05:29 +0800

    rename: index.js -> config.js

--- index.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  semi: false, // default: true
-  printWidth: 120, // default: 80
-  singleQuote: true, // default: false
-  bracketSameLine: true, // default: false
-  htmlWhitespaceSensitivity: 'ignore', // default: 'css'
-}
