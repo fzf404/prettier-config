1a209d27f4962c14991b6e9bde37f81c72133135
fzf404 <me@fzf404.art>
2023-06-25 13:06:17 +0800

    feat: overrides vue and pnpm-lock.yaml

---
+++ config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  semi: false, // default: true
+  printWidth: 120, // default: 80
+  singleQuote: true, // default: false
+  htmlWhitespaceSensitivity: 'ignore', // default: 'css'
+  overrides: [
+    {
+      files: '*.vue',
+      options: {
+        singleAttributePerLine: true, // default: false
+      },
+    },
+    {
+      files: 'pnpm-lock.yaml',
+      options: {
+        rangeEnd: 0, // default: Infinity
+      },
+    },
+  ],
+}
