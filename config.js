bc331b9635b02c69ad04acc299d16731e46a46ff
fzf404 <me@fzf404.art>
2023-06-28 13:17:11 +0800

    feat: use space replace tab

--- config.js
+++ config.js
@@ -1,21 +1,20 @@
 module.exports = {
-	semi: false, // default: true
-	useTabs: true, // default: false
-	printWidth: 120, // default: 80
-	singleQuote: true, // default: false
-	htmlWhitespaceSensitivity: 'ignore', // default: 'css'
-	overrides: [
-		{
-			files: '*.vue',
-			options: {
-				singleAttributePerLine: true, // default: false
-			},
-		},
-		{
-			files: 'pnpm-lock.yaml',
-			options: {
-				requirePragma: true, // default: false
-			},
-		},
-	],
+  semi: false, // default: true
+  printWidth: 120, // default: 80
+  singleQuote: true, // default: false
+  htmlWhitespaceSensitivity: 'ignore', // default: 'css'
+  overrides: [
+    {
+      files: '*.vue',
+      options: {
+        singleAttributePerLine: true, // default: false
+      },
+    },
+    {
+      files: 'pnpm-lock.yaml',
+      options: {
+        requirePragma: true, // default: false
+      },
+    },
+  ],
 }
