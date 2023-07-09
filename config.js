cd5a72dbb25d90d2cef80e330c687165018a282e
fzf404 <me@fzf404.art>
2023-07-10 00:29:47 +0800

    feat: judge prettier version

--- config.js
+++ config.js
@@ -1,8 +1,14 @@
+const { getPackageInfoSync } = require('local-pkg')
+
+const isPrettier2 = getPackageInfoSync('prettier').version.startsWith('2')
+
+/** @type {import('prettier').Config} */
 module.exports = {
   semi: false, // default: true
   printWidth: 120, // default: 80
   singleQuote: true, // default: false
   htmlWhitespaceSensitivity: 'ignore', // default: 'css'
+  plugins: isPrettier2 ? ['prettier-plugin-tailwindcss'] : [], // default: []
   overrides: [
     {
       files: '*.vue',
