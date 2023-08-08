0ffefc8a25fa85b5f95db90efc591618ac3cfafc
fzf404 <me@fzf404.art>
2023-08-08 23:06:53 +0800

    refactor: remove vue attr newline

--- config.js
+++ config.js
@@ -2,15 +2,8 @@
 export default {
   semi: false, // default: true
   singleQuote: true, // default: false
-  trailingComma: 'all', // default: 'es5'
   htmlWhitespaceSensitivity: 'ignore', // default: 'css'
   overrides: [
-    {
-      files: '*.vue',
-      options: {
-        singleAttributePerLine: true, // default: false
-      },
-    },
     {
       files: 'pnpm-lock.yaml',
       options: {
